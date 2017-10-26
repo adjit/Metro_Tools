63eb66b4321ae3d656f49442d5d1607c37203935
adjit <atawil@METRO.corp>
2017-10-26 17:44:50 -0400

    Added LastCC Query
    added a query to get the last credit card number

---
+++ MetroTools/LastCC.sql
@@ -0,0 +1,14 @@
+﻿Select * from Metro.dbo.SOP10103 cc
+Join (
+	select * from Metro.dbo.SOP30200 h
+	join (
+		Select
+			MAX(DOCDATE) Last_Sale
+		From Metro.dbo.SOP30200
+		Where CUSTNMBR = '01001AD' and SOPTYPE = 3
+	) MD
+		on MD.Last_Sale = h.DOCDATE
+		Where h.CUSTNMBR = '01001AD'
+) LS
+
+ON LS.SOPNUMBE = cc.SOPNUMBE
