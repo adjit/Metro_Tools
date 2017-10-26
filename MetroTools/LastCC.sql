b1b91179a4675ffc7c3b568c6702a20af8549788
adjit <atawil@METRO.corp>
2017-10-26 17:52:06 -0400

    Updated Query
    updated the query to select specific columns

--- MetroTools/LastCC.sql
+++ MetroTools/LastCC.sql
@@ -1,14 +1,18 @@
-﻿Select * from Metro.dbo.SOP10103 cc
+﻿Select 
+	cc.SOPNUMBE,
+	cc.CARDNAME,
+	cc.RCTNCCRD
+FROM Metro.dbo.SOP10103 cc
 Join (
 	select * from Metro.dbo.SOP30200 h
 	join (
 		Select
 			MAX(DOCDATE) Last_Sale
 		From Metro.dbo.SOP30200
-		Where CUSTNMBR = '01001AD' and SOPTYPE = 3
+		Where CUSTNMBR = '{0}' and SOPTYPE = 3
 	) MD
 		on MD.Last_Sale = h.DOCDATE
-		Where h.CUSTNMBR = '01001AD'
+		Where h.CUSTNMBR = '{1}'
 ) LS
 
 ON LS.SOPNUMBE = cc.SOPNUMBE
